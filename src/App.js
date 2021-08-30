fef52a82a6b3e700651ef3054fa3d43bce96f627
ruimintan <ruimintan@163.com>
2021-08-31 00:39:13 +0800

    react简单组件

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 // 创建“外壳”组件
 import React, {Component} from 'react';
 
-// 创建类式组件
-class App extends Component{
+// 创建并暴露App组件
+export default class App extends Component{
   render(){
     return(
       <div>
@@ -11,14 +11,3 @@ class App extends Component{
     )
   }
 }
-// 创建函数式组件
-// function App() {
-//   return (
-//     <div>
-//       Hello World
-//     </div>
-//   );
-// }
-
-// 暴露App组件
-export default App;
